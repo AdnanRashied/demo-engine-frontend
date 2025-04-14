75254c20d70b1b9a4d1adfad0122eecd777cdada
AdnanRashied <adnan.rashied@gmail.com>
2025-04-14 18:38:51 -0400

    Create SearchBar.tsx

---
+++ demo-engine-frontend/app/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+type SearchBarProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearch: () => void;
+};
+
+export default function SearchBar({
+  value,
+  onChange,
+  onSearch,
+}: SearchBarProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search User"
+        value={value}
+        onChange={onChange}
+        className="px-4 py-2 rounded-full w-full text-black outline-none"
+      />
+      <button
+        onClick={onSearch}
+        className="px-4 py-2 bg-emerald-700 text-white rounded-full hover:bg-emerald-800 transition"
+      >
+        Search
+      </button>
+    </div>
+  );
+}
