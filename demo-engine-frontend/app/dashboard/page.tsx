c23a3ddfc98e1620e888c45b270f5b6d335b76c3
AdnanRashied <adnan.rashied@gmail.com>
2025-04-21 13:52:14 -0400

    Update page.tsx

--- demo-engine-frontend/app/dashboard/page.tsx
+++ demo-engine-frontend/app/dashboard/page.tsx
@@ -1,22 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { NextResponse } from "next/server";
 
 const Dashboard = () => {
   const router = useRouter();
+
   const handleLogout = async () => {
-    const responseWithClearedCookie = NextResponse.json(
-      { success: true },
-      { status: 200 }
-    );
-    responseWithClearedCookie.headers.append(
-      "Set-Cookie",
-      `auth_token=; HttpOnly; Secure=${
-        process.env.NODE_ENV === "production"
-      }; SameSite=Strict; Path=/; Max-Age=0`
-    );
-    router.push("/login");
+    // Call the logout API to clear the cookie
+    const response = await fetch("/api/logout");
+
+    if (response.ok) {
+      // Redirect to login page after logout
+      router.push("/login");
+    } else {
+      console.error("Logout failed");
+    }
   };
+
   return (
     <div>
       <h1>Dashboard</h1>
